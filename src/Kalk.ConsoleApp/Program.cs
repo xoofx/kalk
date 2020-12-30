fb9e436c619002524764f628daaa0c0d9e1d3149
Alexandre Mutel <alexandre_mutel@live.com>
2020-12-30 21:00:15 +0100

    Try to use net5.0 instead of net5.0-windows10.0.17763.0

--- src/Kalk.ConsoleApp/Program.cs
+++ src/Kalk.ConsoleApp/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.IO;
 using Kalk.Core;
-#if KALK_WINDOWS_STOREAPP
-using Windows.Storage;
-using Windows.System;
-#endif
 
 namespace kalk
 {
@@ -18,28 +13,10 @@ namespace kalk
                 SetClipboardText = TextCopy.ClipboardService.SetText,
             };
 
-#if KALK_WINDOWS_STOREAPP
-            SetupWindowsStoreApp(app);
-#endif
-
             if (!app.Run(args))
             {
                 Environment.ExitCode = 1;
             }
         }
-
-#if KALK_WINDOWS_STOREAPP
-        private static void SetupWindowsStoreApp(KalkEngine app)
-        {
-            try
-            {
-                app.KalkUserFolder = Path.Combine(UserDataPaths.GetDefault().Profile, ".kalk");
-            }
-            catch
-            {
-                // ignore;
-            }
-        }
-#endif
     }
 }
